061776a0abebb6d3401c70c6297ad0faa7c492d6
pearu.peterson <pearu.peterson@376947f0-c9f5-11de-8861-c343c794f7e0>
2009-11-11 12:08:08 +0000

    Added more api docs. Fixed source links.

--- mk_apidocs.sh
+++ mk_apidocs.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-pydoctor -c nidaqmx.cfg --make-html
+pydoctor -c nidaqmx.cfg --make-html --html-write-function-pages --docformat=restructuredtext
+echo Applying 's/.py#L/.py#/g' to all html files to support Google Code.
+perl -p -i -e 's/.py#L/.py#/g' apidocs/*.html
